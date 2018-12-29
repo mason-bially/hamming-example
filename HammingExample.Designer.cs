cf15bf49b31f80980b7f3e7c15941e2a63eac1a2
Mason Bially <mason.bially@gmail.com>
2018-12-28 18:06:03 -0800

    Initial Commit.

---
+++ HammingExample.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class HammingExample
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_0_string = new System.Windows.Forms.TextBox();
+            this.label_0 = new System.Windows.Forms.Label();
+            this.button_0_toBytes = new System.Windows.Forms.Button();
+            this.button_0_fromBytes = new System.Windows.Forms.Button();
+            this.label_1 = new System.Windows.Forms.Label();
+            this.textBox_1_bytes = new System.Windows.Forms.TextBox();
+            this.button_1_encode = new System.Windows.Forms.Button();
+            this.label_2 = new System.Windows.Forms.Label();
+            this.textBox_2_encoded = new System.Windows.Forms.TextBox();
+            this.button_2_decode = new System.Windows.Forms.Button();
+            this.textBox_3_decoded_bytes = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_3_decoded_string = new System.Windows.Forms.TextBox();
+            this.textBox_3_decoded_errors = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button_1_encodeStriped = new System.Windows.Forms.Button();
+            this.button_2_decodeStriped = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_0_string
+            // 
+            this.textBox_0_string.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_0_string.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_0_string.Location = new System.Drawing.Point(121, 12);
+            this.textBox_0_string.Name = "textBox_0_string";
+            this.textBox_0_string.Size = new System.Drawing.Size(461, 18);
+            this.textBox_0_string.TabIndex = 0;
+            // 
+            // label_0
+            // 
+            this.label_0.AutoSize = true;
+            this.label_0.Location = new System.Drawing.Point(12, 13);
+            this.label_0.Name = "label_0";
+            this.label_0.Size = new System.Drawing.Size(34, 13);
+            this.label_0.TabIndex = 1;
+            this.label_0.Text = "String";
+            // 
+            // button_0_toBytes
+            // 
+            this.button_0_toBytes.Location = new System.Drawing.Point(121, 38);
+            this.button_0_toBytes.Name = "button_0_toBytes";
+            this.button_0_toBytes.Size = new System.Drawing.Size(120, 23);
+            this.button_0_toBytes.TabIndex = 2;
+            this.button_0_toBytes.Text = "\\/  To Bytes  \\/";
+            this.button_0_toBytes.UseVisualStyleBackColor = true;
+            this.button_0_toBytes.Click += new System.EventHandler(this.button_0_toBytes_Click);
+            // 
+            // button_0_fromBytes
+            // 
+            this.button_0_fromBytes.Location = new System.Drawing.Point(247, 38);
+            this.button_0_fromBytes.Name = "button_0_fromBytes";
+            this.button_0_fromBytes.Size = new System.Drawing.Size(120, 23);
+            this.button_0_fromBytes.TabIndex = 3;
+            this.button_0_fromBytes.Text = "/\\  From Bytes  /\\";
+            this.button_0_fromBytes.UseVisualStyleBackColor = true;
+            this.button_0_fromBytes.Click += new System.EventHandler(this.button_0_fromBytes_Click);
+            // 
+            // label_1
+            // 
+            this.label_1.AutoSize = true;
+            this.label_1.Location = new System.Drawing.Point(12, 68);
+            this.label_1.Name = "label_1";
+            this.label_1.Size = new System.Drawing.Size(33, 13);
+            this.label_1.TabIndex = 5;
+            this.label_1.Text = "Bytes";
+            // 
+            // textBox_1_bytes
+            // 
+            this.textBox_1_bytes.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_1_bytes.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_1_bytes.Location = new System.Drawing.Point(121, 67);
+            this.textBox_1_bytes.Name = "textBox_1_bytes";
+            this.textBox_1_bytes.Size = new System.Drawing.Size(461, 18);
+            this.textBox_1_bytes.TabIndex = 4;
+            // 
+            // button_1_encode
+            // 
+            this.button_1_encode.Location = new System.Drawing.Point(121, 91);
+            this.button_1_encode.Name = "button_1_encode";
+            this.button_1_encode.Size = new System.Drawing.Size(120, 23);
+            this.button_1_encode.TabIndex = 6;
+            this.button_1_encode.Text = "\\/  Encode  \\/";
+            this.button_1_encode.UseVisualStyleBackColor = true;
+            this.button_1_encode.Click += new System.EventHandler(this.button_1_encode_Click);
+            // 
+            // label_2
+            // 
+            this.label_2.AutoSize = true;
+            this.label_2.Location = new System.Drawing.Point(12, 121);
+            this.label_2.Name = "label_2";
+            this.label_2.Size = new System.Drawing.Size(50, 13);
+            this.label_2.TabIndex = 8;
+            this.label_2.Text = "Encoded";
+            // 
+            // textBox_2_encoded
+            // 
+            this.textBox_2_encoded.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_2_encoded.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_2_encoded.Location = new System.Drawing.Point(121, 120);
+            this.textBox_2_encoded.Name = "textBox_2_encoded";
+            this.textBox_2_encoded.Size = new System.Drawing.Size(461, 18);
+            this.textBox_2_encoded.TabIndex = 7;
+            // 
+            // button_2_decode
+            // 
+            this.button_2_decode.Location = new System.Drawing.Point(121, 144);
+            this.button_2_decode.Name = "button_2_decode";
+            this.button_2_decode.Size = new System.Drawing.Size(120, 23);
+            this.button_2_decode.TabIndex = 9;
+            this.button_2_decode.Text = "\\/  Decode  \\/";
+            this.button_2_decode.UseVisualStyleBackColor = true;
+            this.button_2_decode.Click += new System.EventHandler(this.button_2_decode_Click);
+            // 
+            // textBox_3_decoded_bytes
+            // 
+            this.textBox_3_decoded_bytes.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_3_decoded_bytes.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_3_decoded_bytes.Location = new System.Drawing.Point(121, 173);
+            this.textBox_3_decoded_bytes.Name = "textBox_3_decoded_bytes";
+            this.textBox_3_decoded_bytes.Size = new System.Drawing.Size(461, 18);
+            this.textBox_3_decoded_bytes.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 174);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Bytes";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 222);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "String";
+            // 
+            // textBox_3_decoded_string
+            // 
+            this.textBox_3_decoded_string.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_3_decoded_string.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_3_decoded_string.Location = new System.Drawing.Point(121, 221);
+            this.textBox_3_decoded_string.Name = "textBox_3_decoded_string";
+            this.textBox_3_decoded_string.Size = new System.Drawing.Size(461, 18);
+            this.textBox_3_decoded_string.TabIndex = 13;
+            // 
+            // textBox_3_decoded_errors
+            // 
+            this.textBox_3_decoded_errors.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_3_decoded_errors.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_3_decoded_errors.Location = new System.Drawing.Point(121, 197);
+            this.textBox_3_decoded_errors.Name = "textBox_3_decoded_errors";
+            this.textBox_3_decoded_errors.Size = new System.Drawing.Size(461, 18);
+            this.textBox_3_decoded_errors.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Errors";
+            // 
+            // button_1_encodeStriped
+            // 
+            this.button_1_encodeStriped.Location = new System.Drawing.Point(247, 91);
+            this.button_1_encodeStriped.Name = "button_1_encodeStriped";
+            this.button_1_encodeStriped.Size = new System.Drawing.Size(120, 23);
+            this.button_1_encodeStriped.TabIndex = 16;
+            this.button_1_encodeStriped.Text = "\\/  Stripe Encode  \\/";
+            this.button_1_encodeStriped.UseVisualStyleBackColor = true;
+            this.button_1_encodeStriped.Click += new System.EventHandler(this.button_1_encodeStriped_Click);
+            // 
+            // button_2_decodeStriped
+            // 
+            this.button_2_decodeStriped.Location = new System.Drawing.Point(247, 144);
+            this.button_2_decodeStriped.Name = "button_2_decodeStriped";
+            this.button_2_decodeStriped.Size = new System.Drawing.Size(120, 23);
+            this.button_2_decodeStriped.TabIndex = 17;
+            this.button_2_decodeStriped.Text = "\\/  Stripe Decode  \\/";
+            this.button_2_decodeStriped.UseVisualStyleBackColor = true;
+            this.button_2_decodeStriped.Click += new System.EventHandler(this.button_2_decodeStriped_Click);
+            // 
+            // HammingExample
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 266);
+            this.Controls.Add(this.button_2_decodeStriped);
+            this.Controls.Add(this.button_1_encodeStriped);
+            this.Controls.Add(this.textBox_3_decoded_errors);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_3_decoded_string);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_3_decoded_bytes);
+            this.Controls.Add(this.button_2_decode);
+            this.Controls.Add(this.label_2);
+            this.Controls.Add(this.textBox_2_encoded);
+            this.Controls.Add(this.button_1_encode);
+            this.Controls.Add(this.label_1);
+            this.Controls.Add(this.textBox_1_bytes);
+            this.Controls.Add(this.button_0_fromBytes);
+            this.Controls.Add(this.button_0_toBytes);
+            this.Controls.Add(this.label_0);
+            this.Controls.Add(this.textBox_0_string);
+            this.Name = "HammingExample";
+            this.Text = "Hamming Example";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_0_string;
+        private System.Windows.Forms.Label label_0;
+        private System.Windows.Forms.Button button_0_toBytes;
+        private System.Windows.Forms.Button button_0_fromBytes;
+        private System.Windows.Forms.Label label_1;
+        private System.Windows.Forms.TextBox textBox_1_bytes;
+        private System.Windows.Forms.Button button_1_encode;
+        private System.Windows.Forms.Label label_2;
+        private System.Windows.Forms.TextBox textBox_2_encoded;
+        private System.Windows.Forms.Button button_2_decode;
+        private System.Windows.Forms.TextBox textBox_3_decoded_bytes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_3_decoded_string;
+        private System.Windows.Forms.TextBox textBox_3_decoded_errors;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button_1_encodeStriped;
+        private System.Windows.Forms.Button button_2_decodeStriped;
+    }
+}
+
---
+++ HammingExample.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class HammingExample : Form
+    {
+        public HammingExample()
+        {
+            InitializeComponent();
+        }
+
+        int[] _errors;
+
+        byte[] _hamming_8_4_encode =
+        {
+            0b0000_0000,
+            0b1000_0111,
+            0b1001_1001,
+            0b0001_1110,
+
+            0b1010_1010,
+            0b0010_1101,
+            0b0011_0011,
+            0b1011_0100,
+
+            0b0100_1011,
+            0b1100_1100,
+            0b1101_0010,
+            0b0101_0101,
+
+            0b1110_0001,
+            0b0110_0110,
+            0b0111_1000,
+            0b1111_1111,
+        };
+
+        private byte _hamming_8_4_decode(byte b, out int err)
+        {
+            // Compute syndrome 
+            err = 0;
+            for (int i = 1; i < 8; i++)
+                if (b.GetBit(i - 1)) // is 1
+                    err ^= i;
+
+            // Correct error
+            if (err != 0)
+                b = b.ToggleBit(err - 1);
+
+            // Extract values:
+            b = (byte)(
+                  ((b & 0b00000100) >> 2)
+                + ((b & 0b01110000) >> 3));
+
+            return b;
+        }
+
+        private byte[] HammingEncodeBytes(byte[] in_)
+        {
+            byte[] out_ = new byte[in_.Length * 2];
+
+            // copy data to output buffer
+            for (var i = 0; i < out_.Length; i += 2)
+            {
+                out_[i] = in_[i / 2];
+                out_[i + 1] = in_[i / 2];
+            }
+
+            // update in place
+            for (var i = 0; i < out_.Length; i += 2)
+            {
+                out_[i] = _hamming_8_4_encode[out_[i] & 0b0000_1111];
+                out_[i + 1] = _hamming_8_4_encode[(out_[i + 1] & 0b1111_0000) >> 4];
+            }
+
+            return out_;
+        }
+
+        private byte[] HammingDecodeAndCorrectBytes(byte[] in_)
+        {
+            byte[] out_ = new byte[in_.Length / 2];
+            _errors = new int[in_.Length];
+
+            for (var i = 0; i < out_.Length; i++)
+            {
+                out_[i] = 0;
+                out_[i] += (byte)(_hamming_8_4_decode(in_[i * 2 + 0], out _errors[i * 2 + 0]) << 0);
+                out_[i] += (byte)(_hamming_8_4_decode(in_[i * 2 + 1], out _errors[i * 2 + 1]) << 4);
+            }
+
+            return out_;
+        }
+
+        private byte[] HammingStripedEncodeBytes(byte[] in_)
+        {
+            // Have to pad it:
+            if (in_.Length % 4 != 0)
+            {
+                var new_in = new byte[in_.Length + (4 - (in_.Length % 4))];
+                Buffer.BlockCopy(in_, 0, new_in, 0, in_.Length);
+                for (int i = in_.Length; i < new_in.Length; i++) new_in[i] = 0;
+                in_ = new_in;
+            }
+
+            byte[] out_ = HammingEncodeBytes(in_);
+
+            // Now we stripe it:
+            byte[] temp = new byte[8];
+            for (var i = 0; i < out_.Length; i += 8)
+            {
+                // Copy to temp memory
+                Buffer.BlockCopy(out_, i, temp, 0, 8);
+                
+                // Stripe
+                for (var j = 0; j < 8; j++)
+                {
+                    out_[i + j] = 0;
+                    for (int k = 0; k < 8; k++)
+                    {
+                        out_[i + j] += (byte)((temp[k].GetBit(j) ? 1 : 0) << k);
+                    }
+                }
+            }
+
+            return out_;
+        }
+
+        private byte[] HammingStripedDecodeAndCorrectBytes(byte[] in_)
+        {
+            // First we unstripe it
+            byte[] temp = new byte[8];
+            for (var i = 0; i < in_.Length; i += 8)
+            {
+                // Copy to temp memory
+                Buffer.BlockCopy(in_, i, temp, 0, 8);
+
+                // De-stripe
+                for (var j = 0; j < 8; j++)
+                {
+                    in_[i + j] = 0;
+                    for (int k = 0; k < 8; k++)
+                    {
+                        in_[i + j] += (byte)((temp[k].GetBit(j) ? 1 : 0) << k);
+                    }
+                }
+            }
+
+            byte[] out_ = HammingDecodeAndCorrectBytes(in_);
+
+            return out_;
+        }
+
+        private void _display_errors()
+        {
+            textBox_3_decoded_errors.Text = string.Join(" ",
+                _errors.Select((e, i) => new { i, e })
+                .GroupBy(x => x.i / 2)
+                .Select(g => string.Join("", g.Select(x => x.e.ToString()))));
+        }
+
+        private void button_0_toBytes_Click(object sender, EventArgs e)
+        {
+            string in_text = textBox_0_string.Text;
+
+            byte[] out_bytes = Encoding.ASCII.GetBytes(in_text);
+
+            textBox_1_bytes.Text = out_bytes.ToByteString();
+        }
+
+        private void button_0_fromBytes_Click(object sender, EventArgs e)
+        {
+            byte[] in_bytes = textBox_1_bytes.Text.FromByteString();
+
+            string out_text = Encoding.ASCII.GetString(in_bytes);
+
+            textBox_1_bytes.Text = out_text;
+        }
+
+        private void button_1_encode_Click(object sender, EventArgs e)
+        {
+            byte[] in_bytes = textBox_1_bytes.Text.FromByteString();
+
+            byte[] out_bytes = HammingEncodeBytes(in_bytes);
+
+            textBox_2_encoded.Text = out_bytes.ToByteString();
+        }
+
+        private void button_1_encodeStriped_Click(object sender, EventArgs e)
+        {
+            byte[] in_bytes = textBox_1_bytes.Text.FromByteString();
+
+            byte[] out_bytes = HammingStripedEncodeBytes(in_bytes);
+
+            textBox_2_encoded.Text = out_bytes.ToByteString();
+        }
+
+        private void button_2_decode_Click(object sender, EventArgs e)
+        {
+            byte[] in_bytes = textBox_2_encoded.Text.FromByteString();
+
+            byte[] out_bytes = HammingDecodeAndCorrectBytes(in_bytes);
+
+            textBox_3_decoded_bytes.Text = out_bytes.ToByteString();
+            textBox_3_decoded_string.Text = Encoding.ASCII.GetString(out_bytes);
+            _display_errors();
+        }
+
+        private void button_2_decodeStriped_Click(object sender, EventArgs e)
+        {
+            byte[] in_bytes = textBox_2_encoded.Text.FromByteString();
+
+            byte[] out_bytes = HammingStripedDecodeAndCorrectBytes(in_bytes);
+
+            textBox_3_decoded_bytes.Text = out_bytes.ToByteString();
+            textBox_3_decoded_string.Text = Encoding.ASCII.GetString(out_bytes);
+            _display_errors();
+        }
+    }
+
+    public static class Extension
+    {
+        public static string ToByteString(this byte[] bytes)
+        {
+            return string.Join(" ", bytes.Select(b => b.ToString("X2")));
+        }
+        public static byte[] FromByteString(this string str)
+        {
+            return str.Split(' ').Select(s => Convert.ToByte(s, 16)).ToArray();
+        }
+
+        public static bool GetBit(this byte b, int bitNumber)
+        {
+            return (b & (1 << bitNumber)) != 0;
+        }
+
+        public static byte ToggleBit(this byte b, int bitNumber)
+        {
+            return (byte)(b ^ (1 << bitNumber));
+        }
+    }
+
+}
